1d226b30e8a78fd516beafe252d7f7439fc2bc73
Keith <chen.k@live.com>
2016-03-25 04:08:20 -0700

    index page update

--- cart.php
+++ cart.php
@@ -3,7 +3,7 @@
 
     require_once('./libs/PHPTAL-1.3.0/PHPTAL.php');
 
-    $template = new PHPTAL('shopping_cart.xhtml');
+    $template = new PHPTAL('cart.xhtml');
 
     // now add the variables for processing and that you created from above:
     $template->main_page = "PHOTO4130 CART";
